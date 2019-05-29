d8298a25af1d1fc0dd2b9501aebbfbc9b3f2cbe2
Nicolas Dronchi <dronchin@chemistry.msu.edu>
2019-05-29 12:56:25 -0400

    added comments and improved clarity

--- Two-Gamma Imaging/2Dimensions/2gamma_2D-Reconstruction_flattenZ.py
+++ Two-Gamma Imaging/2Dimensions/2gamma_2D-Reconstruction_flattenZ.py
@@ -2,7 +2,7 @@
 """
 Created on Tue May  9 13:48:58 2017
 
-@author: Nicolas D
+@author: Nicolas Dronchi
 
 The purpose of this program is to take in the detection data and preform a 
 radon transformation on it to construct a sinogram. After the radon 
@@ -15,75 +15,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.fftpack import fft, ifft, fftfreq
 from warnings import warn
-from skimage.transform import warp
 from scipy.interpolate import interp1d
-from IPython import display
-import winsound
-import timeit
-
-starttime = timeit.default_timer()
-
 
 def unpackdata(initial_points_data):
-    ''' This function needs to take a line of data and change it into usable 
-    varibales
-    '''
+    ''' This function needs to take a line of data and change it into usable varibales '''
     detect1 = initial_points_data[0:3]
     detect2 = initial_points_data[3:]
-    
-#    #take x-z projection now
-#    z1 = detect1[2]
-#    z2 = detect2[2]
-#    detect1[1] = z1
-#    detect2[1] = z2
-#    detect1[2] = 0
-#    detect2[2] = 0
+
     return detect1, detect2
 
 def adderror(det1, det2, sigma):
+    ''' This function adds error on to the position detection with a gaussian normal distribution '''
+    # adds a normalized amount of error to each detection
     det1 += np.random.normal(0,scale=sigma, size=3)
     det2 += np.random.normal(0,scale=sigma, size=3)
+    # adds a small amount of background
     if np.random.random() < 0.05:
         det1 += np.random.normal(0,scale=50, size=3)
         det2 += np.random.normal(0,scale=50, size=3)
+    
     return det1, det2
 
 def unpackdata_RD(initial_points_data):
-    ''' This function needs to take a line of data and change it into usable 
-    varibales
-    '''
+    ''' This function needs to take a line of Real Data (RD) and change it into usable 
+    varibales '''
     detect1 = initial_points_data[0:3]
     detect2 = initial_points_data[4:7]
     
-#    #rotate detections by pi/4
-#    x = detect1[0]
-#    y = detect1[1]
-#    detect1[0] = x*np.cos(np.pi/4) - y*np.sin(np.pi/4)
-#    detect1[1] = x*np.sin(np.pi/4) + y*np.cos(np.pi/4)
-##    
-#    x = detect2[0]
-#    y = detect2[1]
-#    detect2[0] = x*np.cos(np.pi/4) - y*np.sin(np.pi/4)
-#    detect2[1] = x*np.sin(np.pi/4) + y*np.cos(np.pi/4)
     return detect1, detect2
-
-def unpackdata_noY(initial_points_data):
-    ''' This function needs to take a line of data and change it into usable 
-    varibales
-    '''
-    detect1 = initial_points_data[0:3]
-    detect2 = initial_points_data[4:7]
-    x1 = detect1[0]
-    x2 = detect2[0]
-    z1 = detect1[2]
-    z2 = detect2[2]
-    detect1[1] = x1
-    detect2[1] = x2
-    detect1[0] = z1 
-    detect2[0] = z2
-    
-    #print(detect1, detect2)
-    return detect1, detect2, z1, z2
 
 def intersect(phi, theta, b1):
     '''This function's goal is to solve the intersection of the axis line and 
@@ -95,14 +54,13 @@ def intersect(phi, theta, b1):
     Output:
         s = distance on the axis. + is right, - is left
     '''
+    # calculate slopes
     m2 = np.tan(phi)
     m1 = np.tan(theta)
-    
+    # calculate position of intercept
     x = b1/(m2-m1)
     y = m2*x
-    
-    #print("x", x, "y",y)
-    
+    # used to 
     if y >= 0:    
         s = np.sqrt(x**2 + y**2)
     else:
@@ -130,10 +88,9 @@ def iradon(radon_image, theta=None, output_size=None,
     Inverse radon transform.
     Reconstruct an image from the radon transform, using the filtered
     back projection algorithm.
-    Parameters
-    
     adapted from scikit-image>transform>radon_transform sourcecode found at:
     https://github.com/scikit-image/scikit-image/blob/master/skimage/transform/radon_transform.py#L127
+    Parameters
     ----------
     radon_image : array_like, dtype=float
         Image containing radon transform (sinogram). Each column of
@@ -203,12 +160,8 @@ def iradon(radon_image, theta=None, output_size=None,
         circle = False
     if circle:
         radon_image = _sinogram_circle_to_square(radon_image)
-        #print(radon_image.shape)
-        #plt.imshow(radon_image, cmap="Greys")
-        #plt.show()
 
     th = (np.pi / 180.0) * theta
-    #print(th)
     # resize image to next power of two (but no less than 64) for
     # Fourier analysis; speeds up Fourier and lessens artifacts
     projection_size_padded = \
@@ -269,12 +222,10 @@ def iradon(radon_image, theta=None, output_size=None,
         radius = output_size // 2
         reconstruction_circle = (xpr ** 2 + ypr ** 2) <= radius ** 2
         reconstructed[~reconstruction_circle] = 0.
-    print("it's working")
+
     return reconstructed * np.pi / (2 * len(th))
 
 
-
-slopelist = [] #stores the slope values
 filelist = ['detector datasets/PET_03_r53.dat', 'detector datasets/PET_06_r52.dat',
             'detector datasets/PET_09_r54.dat', 'detector datasets/PET_12_r50.dat',
             'detector datasets/PET_center_r48.dat'] #'PET_06_r51.dat', 
@@ -317,22 +268,31 @@ filelist23 = ["simulate_moved_Na_downshifted_15.txt"]
 filelist24 = ["simulate_point_source.txt"]
 filelist25 = ['simulate_phantom_temp.txt']
 
+#==============================================================================
+# Main Program
+#==============================================================================
+
+slopelist = [] #stores the slope values
 zrange = []    
-for filenum, filename in enumerate(filelist25):
+for filenum, filename in enumerate(filelist):
     print(filenum)
     with open("C:/data/{}".format(filename, "r")) as data:
         for count,line in enumerate(data):
-#            if count > 78733:
-#                print("cut", count)
-#                break
+            # used in the experimental data to make each point equal in 
+            if count > 78733:
+                print("cut", count)
+                break
             #unpacks the data and stores them in a list
             numbers_float = map(float, line.split())
             initial_points_data = list(numbers_float)
-            #print(initial_points_data)
-            detect1, detect2 = unpackdata(initial_points_data) #saves data as detect1 and dectect2
-            detect1, detect2 = adderror(detect1, detect2, 3)
-
+            #for using experimental data use this version of unpack
+            detect1, detect2 = unpackdata_RD(initial_points_data) #saves data as detect1 and dectect2
             
+            #add on error and change unpack if using simulated data
+#            detect1, detect2 = unpackdata(initial_points_data)
+#            detect1, detect2 = adderror(detect1, detect2, 3)
+
+            #
             if detect2[0] - detect1[0] == 0:
                 continue
                         
@@ -342,8 +302,6 @@ for filenum, filename in enumerate(filelist25):
             slope = np.arctan(m) #calculates the slope in angles form
             
             slopelist.append([slope,b])
-        print(count)
-
       
 
 phistep = np.pi/100
@@ -358,41 +316,31 @@ plt.show()
 
 
 #Need to serach and sort through the angles and group them by their angles
+#I'm sure there is a much better way to sort through these but this works
+#step through every phi bin
 for phi in np.arange(0, np.pi + phistep, phistep):
     normphi = phi - np.pi/2 #need to be parallel to the normal angle
-#    print(phi)
+    #step through every detection
     for theta,b1 in slopelist:
+        #put in bin if conditions are correct
         if (theta > normphi and theta < normphi+phistep):
             #uses interesect function to find the distance on the new rotated axis
             s = intersect(phi, theta, b1) 
-            #print("s", s)
             angles.append(phi)
             slist.append(s)
             
-#
-#stoptime = timeit.default_timer()
-#print("time taken: ", stoptime - starttime)  
-#
-#
 #A histogram will group together all the s values and weight them accordingly to form a sinogram 
-heatmap, xedges, yedges = np.histogram2d(slist, angles,bins=[np.arange(-60,61,1),np.linspace(0,np.pi,100)], normed=True)
-##uses the iradon function to turn a sinogram into a reconstructed image using back projection
+heatmap, xedges, yedges = np.histogram2d(slist, angles, 
+                                         bins=[np.arange(-60,61,1), np.linspace(0,np.pi,100)], normed=True)
+
+#uses the iradon function to turn a sinogram into a reconstructed image using back projection
 reconstruction = iradon(heatmap, circle=True, filter="ramp")
 
 reconstruction -= reconstruction.min()
 reconstruction = reconstruction/reconstruction.max()
-#reconstruction = reconstruction.T
+reconstruction = reconstruction.T
 reconstruction = reconstruction[::-1, ...]
 
-
-#np.save("reconstruction_upstream.npy", reconstruction)
-
-#
-##plots the sinogram and reconstructed image
-#plt.figure(figsize=(8.5,11))
-##plt.subplot(2,1,1)
-#plt.imshow(heatmap, cmap="Greys")
-##plt.subplot(2,1,2)
 plt.style.use('default')
 
 plt.imshow(reconstruction, cmap="Greys", origin='lower')
@@ -405,7 +353,5 @@ plt.title('Simulated Phantom')
 #plt.imshow(heatmap) 
 #plt.savefig("Sim_phantom_1.png", dpi=1200, format='png') 
 plt.show()
-
-winsound.Beep(800,1000)  
     
 #        
